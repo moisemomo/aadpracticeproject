6c8e7154a9b8eec872e4613bd1553f1f5c13c62e
momond3 <momond3@gmail.com>
2020-09-13 13:57:05 +0000

    Create Splash Screen View - Renamed MainActivity.kt by SplashActivity.kt and activity_main.xml by activity_splash.xml - Add splash screen icon in activity_splash.xml - Set colors for theme in colors.xml

---
+++ app/src/main/java/sn/moisemomo/aadpracticeproject/SplashActivity.kt
@@ -0,0 +1,22 @@
+package sn.moisemomo.aadpracticeproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        openHomeActivity()
+    }
+
+    private fun openHomeActivity() {
+        Handler().postDelayed({
+                startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            }, 2000
+        )
+    }
+}
